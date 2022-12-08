0a98dab0c7ba49d8ddf5372a79ea65cabd76b072
Dragan Golic <golicdragan0@gmail.com>
2022-12-08 10:53:46 -0500

    dr.code

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
+from kivy.graphics.vertex_instructions import Color
+from kivy.graphics.vertex_instructions import Line
+from kivy.lang import Builder
+from kivy.metrics import dp
 from kivy.uix.widget import Widget
+#from kivy.uix.screenmanager import ScreenManager
+#from View.screens import screens
 
 
 class WidgetsExample(GridLayout):
@@ -74,8 +80,56 @@ class MainWidget(Widget):
     pass
 
 
+# class MyScreenManager(NavigationScreenManager):
+#     pass
+#
+#
+# class TheLabApp(App):
+#     manager = ObjectProperty(None)
+#
+#     def build(self):
+#         self.manager = MyScreenManager()
+#         # return self.manager
+#         return CanvasExample2()
+
 class TheLabApp(App):
     pass
+
+
+class CanvasExample1(Widget):
+    pass
+
+
+class CanvasExample2(Widget):
+    pass
+
+
+class CanvasExample3(Widget):
+    pass
+
+
+class CanvasExample4(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas:
+            Line(points=(100, 100, 400, 500), width=2)
+            Color(0, 1, 0)
+            Line(circle=(400, 200, 80), width=2)
+            Line(rectangle=(700, 500, 150, 100), width=5)
+            self.rect = Rectangle(pos=(700, 200), size=(150, 100))
+            
+
+    def on_button_a_click(self):
+        #print("foo")
+        x, y = self.rect.pos
+        w, h = self.rect.size
+        inc = dp(10)
+        dif = self.width - (x+w)
+
+        if dif < inc:
+            inc = diff
+        x += dp(10)
+        self.rect.pos = (x, y)
 
 
 if __name__ == "__main__":
